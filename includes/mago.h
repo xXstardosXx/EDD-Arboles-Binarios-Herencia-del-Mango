6d647696b19aed34a75e75bdcb696b7146b0790c
xXstardosXx <112598799+xXstardosXx@users.noreply.github.com>
2025-07-27 20:59:55 -0400

    update mago.h
    se le cambio el nombre a las variables

--- includes/mago.h
+++ includes/mago.h
@@ -11,18 +11,18 @@ class Mago{
         string genero;
         int edad;
         int id_padre;
-        bool esta_muerto;
+        bool estado;
         string tipo_magia;
         bool es_dueño;
-
-        Mago(int _id, string _nombre, string _apellido, string _genero, int _edad, int _id_padre, bool _esta_muerto, string _tipo_magia, bool _es_dueño){
+        
+        Mago(int _id, string _nombre, string _apellido, string _genero, int _edad, int _id_padre, bool _estado, string _tipo_magia, bool _es_dueño){
             id = _id;
             nombre = _nombre;
             apellido = _apellido;
             genero = _genero;
             edad = _edad;
             id_padre = _id_padre;
-            esta_muerto = _esta_muerto;
+            estado = _estado;
             tipo_magia = _tipo_magia;
             es_dueño = _es_dueño;
         }
